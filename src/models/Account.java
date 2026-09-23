887e928ca5ee29437611bcd8144fdbf570a264cb
vito-ctrl <aymane.elkhadraoui1@gmai.com>
2026-09-23 11:16:26 +0000

    feat : Account model, with sealed to oly two classes and inheiret this class

---
+++ src/models/Account.java
@@ -0,0 +1,37 @@
+package model;
+
+public sealed abstract class Account 
+        permits CurrentAccount, SavingsAccount {
+    
+    private long id;
+    private String number;
+    private double balance;
+    private Long clientId;
+
+    public Account(Long id, String number, double balance, Long clientId) {
+        this.id = id;
+        this.number = number;
+        this.balance = balance;
+        this.clientId = clientId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
