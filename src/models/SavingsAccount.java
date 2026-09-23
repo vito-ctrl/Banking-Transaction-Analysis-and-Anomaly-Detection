c70c8d206c722d270341375a1783b58bf614fb67
vito-ctrl <aymane.elkhadraoui1@gmai.com>
2026-09-23 11:26:25 +0000

    feat : saving Account, that extand Account

---
+++ src/models/SavingsAccount.java
@@ -0,0 +1,19 @@
+package mdoel;
+
+public final class SavingsAccount extends Accout {
+    private double interestRate;
+
+    public SavingsAccount(Long id, String number, double balance, Long clientId, double interestRate) {
+
+        super(id, number, balance, clientId);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+}
